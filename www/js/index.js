76b95b9cc1bcc745bcb4769e5256807d7736915c
Alex Ganderton <gana1_17@uni.worc.ac.uk>
2018-10-09 18:04:30 +0100

    Information Added WK3
    Info for Event listeners and Handlers

--- www/js/index.js
+++ www/js/index.js
@@ -2,9 +2,10 @@ var paused_count =0;
 var resumed_count = 0;
 var launched_count = 0;
 
+// An event listener is Defined
 document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
+
+
 function updateDisplay() {
 	$("#launched").text("Application launched: " + launched_count);
 	$("#resumed").text("Application paused: " + paused_count);
@@ -15,30 +16,34 @@ function updateDisplay() {
 // device APIs are available
 //
     function onDeviceReady() {
-	
+
+// Another event listener is Defined as are Lifecycle events - onResume and onPause
+// These handlers check for different states that the phone is in depending on what the user is doing on the device
+// They then execute the commands based on those states
+
 	document.addEventListener("resume", onResume, false);
 	document.addEventListener("pause", onPause, false);
-	
+
 	launched_count++;
 	updateDisplay();
-	    
+
 	alert("device ready");
     }
 
 
     function onPause() {
-	
+
 	paused_count++;
 	updateDisplay();
-	    
+
 	alert("pause");
     }
-	
+
 
     function onResume() {
-		
+
 	resumed_count++;
 	updateDisplay();
-	    
+
 	alert("resume");
     }
